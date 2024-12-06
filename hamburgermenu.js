1efd91eba89bda7f6a6be23cfaeddaf3be8e81b5
designtjsc <tjsc61999@edu.ucl.dk>
2024-12-06 13:53:04 +0100

    burgermenu funktion i js

---
+++ hamburgermenu.js
@@ -0,0 +1,41 @@
+const sidebar = document.querySelector('.sidebar');
+
+function showSidebar(){
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    sidebar.style.display = 'none'
+}
+
+
+const menuItems = [
+    {   listItem: "Tidligere projekter",
+        toUrl: "index.html" // Relativ URL     
+    },
+
+    {   listItem: "Guide",
+        toUrl: "guide.html"
+     },
+
+    {   listItem: "Kommende kurser",
+      toUrl: "courses.html"
+    }
+  ];
+  
+  
+  function addMenuItems(root, data) {
+    let menuItemsList = document.createElement("ul");
+    root.appendChild(menuItemsList);
+    
+    let listItem = document.createElement("li");
+    listItem.innerHTML = "<a href='"+ data.toUrl +"'>"+ data.listItem + "</a>";
+    menuItemsList.appendChild(listItem);
+  }
+  
+  if (sidebar != null) {
+    for (let i = 0; i < menuItems.length; i++) {
+        addMenuItems(sidebar, menuItems[i]);
+    }
+  }
+
